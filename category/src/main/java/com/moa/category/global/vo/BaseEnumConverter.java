f1716171c8e84637b89719e7e642f3c66ad9f3c5
DESKTOP-RL5EITB\교육생62 <003julie@gmail.com>
2023-10-23 14:18:35 +0900

    [feat]category 초기환경세팅

--- category/src/main/java/com/moa/category/global/vo/BaseEnumConverter.java
+++ category/src/main/java/com/moa/category/global/vo/BaseEnumConverter.java
@@ -1,26 +1,7 @@
 package com.moa.category.global.vo;
 
 
-import com.moa.meeting.domain.enums.JoinGender;
-import com.moa.meeting.domain.enums.MeetingStatus;
-
-
 public class BaseEnumConverter {
 
-	public static class JoinGenderConverter extends AbstractBaseEnumConverter<JoinGender, Character, String> {
-
-		public JoinGenderConverter() {
-			super(JoinGender.class);
-		}
-
-	}
-
-	public static class MeetingStatusConverter extends AbstractBaseEnumConverter<MeetingStatus, String, String> {
-
-		public MeetingStatusConverter() {
-			super(MeetingStatus.class);
-		}
-
-	}
 
 }
