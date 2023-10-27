97b74cf77adec8d6f717609a5bb1f8339cde701f
a0kim <94776040+a0Kim@users.noreply.github.com>
2023-10-27 14:52:44 +0900

    [feat.category를 개발을 위한 domain작성, dto작성, repository 작성]

--- category/src/main/java/com/moa/category/category/dto/ThemeCategoryDto.java
+++ category/src/main/java/com/moa/category/category/dto/ThemeCategoryDto.java
@@ -12,6 +12,6 @@ public class ThemeCategoryDto {
     private Integer themeCategoryId;    //주제 카테고리 Id
     private String categoryName;    // 주제 카테고리 이름
     private Boolean categoryUse;    //카테고리 사용여부 1: 사용 0:사용X
-//    private Integer categoryId;     //상위카테고리 id -> 자기참조
+    private Integer categoryId;     //상위카테고리 id -> 자기참조
     //todo : 자기 참조 일경우에도 dto를 작성해야하나요 ?
 }
